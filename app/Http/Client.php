9c7f6f9a74c322493cf1ea7504122d0df543771a
Ben <benhsia@awoo.com.tw>
2019-09-03 22:04:40 +0800

    新增 options 參數

--- app/Http/Client.php
+++ app/Http/Client.php
@@ -8,9 +8,10 @@ class Client
      * 送出 post 請求
      *
      * @param string $url
+     * @param array $options
      * @return string
      */
-    public function post($url)
+    public function post($url, $options)
     {
         // 還沒實作 0.0
     }
